c43b6f9d4f06b0bdbd69119c10c4aaf53a244ebf
Hugo Maitre <hugo.maitre69@gmail.com>
2018-10-03 11:46:48 +0200

    Creation GameState

---
+++ src/main/scala/battleship/core/GameState.scala
@@ -0,0 +1,24 @@
+package battleship.core
+
+import battleship.core.models
+import battleship.core.models.PlayerTrait
+
+case class GameState(currentPlayer: PlayerTrait, opponent: PlayerTrait, numberOfTurns: Int) {
+
+  def isThereAWinner(): Option[PlayerTrait] = {
+    if(currentPlayer.numberOfShipsLeft() == 0){
+      return Option[PlayerTrait](currentPlayer)
+    } else if(opponent.numberOfShipsLeft() == 0){
+      return Option[PlayerTrait](opponent)
+    } else {
+      return None
+    }
+  }
+
+
+}
+
+object GameState {
+
+  def apply(currentPlayer: PlayerTrait, opponent: PlayerTrait, numberOfTurns: Int): GameState = new GameState(currentPlayer, opponent, numberOfTurns)
+}
